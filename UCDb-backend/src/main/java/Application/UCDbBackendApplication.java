f88a9135ca769033ab7a5d2b83dd6007b9b3ef81
Eduardoccufcg <40031298+Eduardoccufcg@users.noreply.github.com>
2019-06-14 00:30:17 -0300

    Criando a estrutura básica do backend.
    Criando a estrutura básica do backend.

---
+++ UCDb-backend/src/main/java/Application/UCDbBackendApplication.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UCDbBackendApplication.class, args);
+	}
+
+}
