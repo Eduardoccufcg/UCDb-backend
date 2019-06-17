c0b1ab9d034edae46d9d847e19122ddb321b126b
juliafealves <giT#8985>
2019-06-17 19:57:23 -0300

    Reformatado os arquivos e adicionado CORS para acesso do front-end.

--- UCDb-backend/src/main/java/Application/UCDbBackendApplication.java
+++ UCDb-backend/src/main/java/Application/UCDbBackendApplication.java
@@ -6,28 +6,38 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import Application.security.TokenFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
 public class UCDbBackendApplication {
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean
-	public FilterRegistrationBean filterJwt() {
-		FilterRegistrationBean filterRb = new FilterRegistrationBean();
-		filterRb.setFilter(new TokenFilter());
-		filterRb.setEnabled(false);
-	
-		// Será usado para filtrar futuras rotas --> filterRb.addUrlPatterns("/private");
-		
-		return filterRb;
 
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(UCDbBackendApplication.class, args);
-	}
-	
-	
-	
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Bean
+    public FilterRegistrationBean filterJwt() {
+        FilterRegistrationBean filterRb = new FilterRegistrationBean();
+        filterRb.setFilter(new TokenFilter());
+        filterRb.setEnabled(false);
 
+        // Será usado para filtrar futuras rotas --> filterRb.addUrlPatterns("/private");
+        return filterRb;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+
+        return bean;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UCDbBackendApplication.class, args);
+    }
 }
